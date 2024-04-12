5bb9867468bed44bca3496463f768b104b59ad1b
Selvamozhi S <selvamozhi1997@gmail.com>
2024-04-12 14:31:41 +0530

    component created, styling added and Routing added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import Main from './Main';
 import Nav from './Nav';
 import Footer from './Footer';
 import Header from './Header';
-
+import { Routes, Route} from 'react-router-dom';
+import {About,OrderOnline,Menu,Login} from './Sections';
+import Reservation from './Reservation';
 function App() {
   return (
     <>
     <Header></Header>
     <Nav></Nav>
-    <Main></Main>
+   
+    <Routes>
+      <Route path="/" element={<Main/>}/>
+      <Route path="/home" element={<Main/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/orderOnline" element={<OrderOnline/>}/>
+      <Route path="/menu" element={<Menu/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/reservation" element={<Reservation/>}/>
+    </Routes>
     <Footer></Footer>
     </>
   );
---
+++ src/Sections.js
@@ -0,0 +1,16 @@
+function About(){
+    return(<>About Screen</>)
+}
+
+function OrderOnline(){
+    return(<>OrderOnline</>)
+}
+function Menu(){
+    return(<>Menu screen</>)
+}
+
+function Login(){
+    return(<>login</>)
+}
+
+export {About,OrderOnline,Menu,Login}
